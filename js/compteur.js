5a0ce13786a89a9469712a3662450ab4181e2eea
Cynthia M <32307333+CynthiaM98@users.noreply.github.com>
2019-12-29 22:05:34 +0100

    okok

--- js/compteur.js
+++ js/compteur.js
@@ -1,14 +1,26 @@
+
+
 document.addEventListener('DOMContentLoaded', function(event){
 
-	var compte_a_rebours = document.getElementById("compte_a_rebours");
-	var dateActuelle = new Date();
-	var dateDebut = new Date("Jan 31 18:00:00 2020");
+	var nbJour = document.getElementById("nbJour");
+	var nbHeure = document.getElementById("nbHeure");
+	var nbMinute = document.getElementById("nbMinute");
+	var nbSeconde = document.getElementById("nbSeconde");
 	var total_secondes = 0;
 	var prefixe = "";
 
-	setInterval(function () {
+	function goodFormat(nb){
+		if (nb < 10){
+			return "0" + nb;
+		}
+		return nb;
+	}
+
+	function compteurLoop(){
+		var dateActuelle = new Date();
+		var dateDebut = new Date("Jan 31 18:00:00 2020");
 		total_secondes = (dateDebut - dateActuelle) / 1000;
-		prefixe = "La Global Game Jam commence dans ";
+		prefixe = "LA GLOBAL GAME JAM ";
 
 		if (total_secondes < 0){
 
@@ -25,11 +37,17 @@ document.addEventListener('DOMContentLoaded', function(event){
 		    var heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
 		    var minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
 			var secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
-			compte_a_rebours.innerHTML = prefixe + jours + ' j ' + heures + ' h ' + minutes + ' min ' + secondes + ' sec';
+
+			nbJour.innerHTML = jours;
+			nbHeure.innerHTML = goodFormat(heures);
+			nbMinute.innerHTML = goodFormat(minutes);
+			nbSeconde.innerHTML = goodFormat(secondes);
 		}
+	}
+
+	setInterval(compteurLoop, 1000);
 
 
-	}, 1000)
 
 });
 
